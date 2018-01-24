8761b0e722678a721b9e998b5de34700c43c9c71
kimkundad <kim.kundad@gmail.com>
2018-01-25 05:28:27 +0700

    golf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,76 +5,30 @@
 
 
   <div class="section section-basic">
+    <div class="space-70"></div>
       <div class="container">
           <h3 class="title">Basic Elements</h3>
-          <div id="buttons">
-              <h4>Buttons</h4>
-              <p class="category">Pick your style</p>
-              <div class="row">
-                  <div class="col-md-10">
-                      <button class="btn btn-primary" type="button">Default</button>
-                      <button class="btn btn-primary btn-round" type="button">Round</button>
-                      <button class="btn btn-primary btn-round" type="button">
-                          <i class="now-ui-icons ui-2_favourite-28"></i> With Icon
-                      </button>
-                      <button class="btn btn-primary btn-icon btn-round" type="button">
-                          <i class="now-ui-icons ui-2_favourite-28"></i>
-                      </button>
-                      <button class="btn btn-primary btn-simple btn-round" type="button">Simple</button>
-                  </div>
-              </div>
-              <p class="category">Pick your size</p>
-              <div class="row">
-                  <div class="col-md-10">
-                      <button class="btn btn-primary btn-sm">Small</button>
-                      <button class="btn btn-primary">Regular</button>
-                      <button class="btn btn-primary btn-lg">Large</button>
-                  </div>
-              </div>
-              <p class="category">Pick your color</p>
-              <div class="row">
-                  <div class="col-md-10">
-                      <button class="btn">Default</button>
-                      <button class="btn btn-primary">Primary</button>
-                      <button class="btn btn-info">Info</button>
-                      <button class="btn btn-success">Success</button>
-                      <button class="btn btn-warning">Warning</button>
-                      <button class="btn btn-danger">Danger</button>
-                      <button class="btn btn-neutral">Neutral</button>
-                  </div>
-              </div>
-              <h4>Links</h4>
-              <div class="row">
-                  <div class="col-md-8">
-                      <button class="btn btn-link">Default</button>
-                      <button class="btn btn-link btn-primary ">Primary</button>
-                      <button class="btn btn-link btn-info">Info</button>
-                      <button class="btn btn-link btn-success">Success</button>
-                      <button class="btn btn-link btn-warning">Warning</button>
-                      <button class="btn btn-link btn-danger">Danger</button>
-                  </div>
-              </div>
-          </div>
+
           <div id="inputs">
               <h4>Inputs</h4>
               <p class="category">Form Controls</p>
               <div class="row">
-                  <div class="col-sm-6 col-lg-3">
+                  <div class="col-md-4">
                       <div class="form-group">
                           <input type="text" value="" placeholder="Regular" class="form-control" />
                       </div>
                   </div>
-                  <div class="col-sm-6 col-lg-3">
+                  <div class="col-md-4">
                       <div class="form-group has-success">
                           <input type="text" value="Success" class="form-control form-control-success" />
                       </div>
                   </div>
-                  <div class="col-sm-6 col-lg-3">
+                  <div class="col-md-4">
                       <div class="form-group has-danger">
                           <input type="email" value="Error Input" class="form-control form-control-danger" />
                       </div>
                   </div>
-                  <div class="col-sm-6 col-lg-3">
+                  <div class="col-md-4">
                       <div class="input-group">
                           <span class="input-group-addon">
                               <i class="fa fa-user-circle"></i>
@@ -82,7 +36,121 @@
                           <input type="text" class="form-control" placeholder="Left Font Awesome Icon">
                       </div>
                   </div>
-                  <div class="col-sm-6 col-lg-3">
+                  <div class="col-md-4">
+                      <div class="input-group">
+                          <input type="text" class="form-control" placeholder="Right Nucleo Icon">
+                          <span class="input-group-addon">
+                              <i class="now-ui-icons users_single-02"></i>
+                          </span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div id="inputs">
+              <h4>Inputs</h4>
+              <p class="category">Form Controls</p>
+              <div class="row">
+                  <div class="col-md-4">
+                      <div class="form-group">
+                          <input type="text" value="" placeholder="Regular" class="form-control" />
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="form-group has-success">
+                          <input type="text" value="Success" class="form-control form-control-success" />
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="form-group has-danger">
+                          <input type="email" value="Error Input" class="form-control form-control-danger" />
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="input-group">
+                          <span class="input-group-addon">
+                              <i class="fa fa-user-circle"></i>
+                          </span>
+                          <input type="text" class="form-control" placeholder="Left Font Awesome Icon">
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="input-group">
+                          <input type="text" class="form-control" placeholder="Right Nucleo Icon">
+                          <span class="input-group-addon">
+                              <i class="now-ui-icons users_single-02"></i>
+                          </span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div id="inputs">
+              <h4>Inputs</h4>
+              <p class="category">Form Controls</p>
+              <div class="row">
+                  <div class="col-md-4">
+                      <div class="form-group">
+                          <input type="text" value="" placeholder="Regular" class="form-control" />
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="form-group has-success">
+                          <input type="text" value="Success" class="form-control form-control-success" />
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="form-group has-danger">
+                          <input type="email" value="Error Input" class="form-control form-control-danger" />
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="input-group">
+                          <span class="input-group-addon">
+                              <i class="fa fa-user-circle"></i>
+                          </span>
+                          <input type="text" class="form-control" placeholder="Left Font Awesome Icon">
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="input-group">
+                          <input type="text" class="form-control" placeholder="Right Nucleo Icon">
+                          <span class="input-group-addon">
+                              <i class="now-ui-icons users_single-02"></i>
+                          </span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div id="inputs">
+              <h4>Inputs</h4>
+              <p class="category">Form Controls</p>
+              <div class="row">
+                  <div class="col-md-4">
+                      <div class="form-group">
+                          <input type="text" value="" placeholder="Regular" class="form-control" />
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="form-group has-success">
+                          <input type="text" value="Success" class="form-control form-control-success" />
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="form-group has-danger">
+                          <input type="email" value="Error Input" class="form-control form-control-danger" />
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="input-group">
+                          <span class="input-group-addon">
+                              <i class="fa fa-user-circle"></i>
+                          </span>
+                          <input type="text" class="form-control" placeholder="Left Font Awesome Icon">
+                      </div>
+                  </div>
+                  <div class="col-md-4">
                       <div class="input-group">
                           <input type="text" class="form-control" placeholder="Right Nucleo Icon">
                           <span class="input-group-addon">
