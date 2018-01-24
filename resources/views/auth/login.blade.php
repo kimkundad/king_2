b6ac7232961eda226dc5701ddcd711057e111179
kimkundad <kim.kundad@gmail.com>
2018-01-25 02:06:50 +0700

    golf

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,34 +13,45 @@ if(Auth::check()){
 
 
 <div class="page-header" filter-color="orange">
+
     <div class="page-header-image" style="background-image:url({{asset('/assets/img/login.jpg')}})"></div>
+
+
     <div class="container">
         <div class="col-md-4 content-center">
             <div class="card card-login card-plain">
-                <form class="form" method="" action="">
+
+                <form class="form" method="POST" action="{{ url('/login') }}">
+                  {{ csrf_field() }}
                     <div class="header header-primary text-center">
                         <div class="logo-container">
                             <img src="{{asset('/assets/img/logo/linchak-logo.png')}}" alt=""  >
                         </div>
                     </div>
                     <div class="content">
-                        <div class="input-group form-group-no-border input-lg">
+
+                        <div class="input-group form-group-no-border input-lg has-danger">
                             <span class="input-group-addon">
                                 <i class="now-ui-icons ui-1_email-85"></i>
                             </span>
-                            <input type="text" class="form-control" placeholder="Emain Address...">
+                            <input type="text" class="form-control form-control-danger" placeholder="Emain Address..." name="email" value="{{ old('email') }}">
                         </div>
-                        <div class="input-group form-group-no-border input-lg" style="margin-bottom: 24px;">
+                        <div class="input-group form-group-no-border input-lg" style="margin-bottom: 20px;">
                             <span class="input-group-addon">
                                 <i class="now-ui-icons ui-1_lock-circle-open"></i>
                             </span>
-                            <input type="password" placeholder="Password..." class="form-control" />
+                            <input type="password" placeholder="Password..." name="password" class="form-control" />
                         </div>
-                    </div>
-                    <div class="footer text-center">
-                        <a href="#pablo" class="btn btn-primary btn-round btn-lg btn-block">Get Started</a>
 
-                        <a href="#pablo" style="margin-top:15px;" class="btn btn-facebook btn-round btn-lg btn-block"><i class="fa fa-facebook"></i> Connect with Facebook</a>
+                        <p class="text-danger" style="font-size:11px; margin-bottom: 0px;">
+                                        "Can not login, username/password is incorrect"
+                                    </p>
+
+                    </div>
+                    <div class="footer text-center" style="padding-top:20px">
+
+                        <button type="submit" class="btn btn-primary btn-round btn-lg btn-block">Get Started</button>
+                        <a href="#pablo" style="margin-top:10px;" class="btn btn-facebook btn-round btn-lg btn-block"><i class="fa fa-facebook"></i> Connect with Facebook</a>
                     </div>
                     <div class="pull-left">
                         <h6>
@@ -53,6 +64,9 @@ if(Auth::check()){
                         </h6>
                     </div>
                 </form>
+
+
+
             </div>
         </div>
     </div>
