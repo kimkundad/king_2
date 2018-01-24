4ce044be2ea60a5db296669d3f61bdc4ab45b56f
kimkundad <kim.kundad@gmail.com>
2018-01-25 04:56:10 +0700

    golf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,28 +4,6 @@
 
 
 
-
-
-
-<div class="section section-images">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-12">
-                  <div class="hero-images-container">
-                      <img src="{{asset('/assets/img/hero-image-1.png')}}" alt="">
-                  </div>
-                  <div class="hero-images-container-1">
-                      <img src="{{asset('/assets/img/hero-image-2.png')}}" alt="">
-                  </div>
-                  <div class="hero-images-container-2">
-                      <img src="{{asset('/assets/img/hero-image-3.png')}}" alt="">
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
-
-
   <div class="section section-basic">
       <div class="container">
           <h3 class="title">Basic Elements</h3>
