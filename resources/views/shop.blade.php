9c07d4dd734e6cee828051900fd7999b6207ee8b
ku-zero <kim.kundad@gmail.com>
2018-02-11 02:01:04 +0700

    golfฃ

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -173,12 +173,18 @@ font-weight: 500;
                                 <div class="col-md-10 ml-auto mr-auto">
                                     <div class="row collections">
                                         <div class="col-md-6">
-                                            <img src="../assets/img/bg6.jpg" class="img-raised">
-                                            <img src="../assets/img/bg11.jpg" alt="" class="img-raised">
+                                            <p class="category">Tabs with Icons on Card</p>
+
+                                            <img src="{{url('assets/img/bg8.jpg')}}" alt="" class="img-raised mar-bot">
+
+                                            <p class="category">Tabs with Icons on Card</p>
+                                            <img src="{{url('assets/img/bg11.jpg')}}" alt="" class="img-raised mar-bot">
                                         </div>
                                         <div class="col-md-6">
-                                            <img src="../assets/img/bg7.jpg" alt="" class="img-raised">
-                                            <img src="../assets/img/bg8.jpg" alt="" class="img-raised">
+                                          <p class="category">Tabs with Icons on Card</p>
+                                            <img src="{{url('assets/img/bg7.jpg')}}" alt="" class="img-raised mar-bot">
+                                            <p class="category">Tabs with Icons on Card</p>
+                                            <img src="{{url('assets/img/bg6.jpg')}}" class="img-raised mar-bot">
                                         </div>
                                     </div>
                                 </div>
