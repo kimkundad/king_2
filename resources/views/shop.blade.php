d7ff5214e6c7d80985d7fd80a7749fe1d399957d
kimkundad <kim.kundad@gmail.com>
2018-03-09 20:31:08 +0700

    golf

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -275,4 +275,45 @@ font-weight: 500;
 
 
 
+                                  <div class="wrapper">
+                                    <footer class="footer">
+                                        <div class="container">
+                                          <p class="text-muted"> </p>
+                                            <nav>
+                                                <ul>
+                                                    <li>
+                                                        <a href="#">
+                                                            ข้อกำหนดในการให้บริการของไซต์
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#">
+                                                            นโยบายความเป็นส่วนตัว
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#">
+                                                            เกี่ยวกับ Linchak
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#">
+                                                            ข้อกำหนดในการให้บริการ
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </nav>
+                                            <div class="copyright">
+                                                ©
+                                                <script>
+                                                    document.write(new Date().getFullYear())
+                                                </script>2018, Designed by
+                                                <a href="#" target="_blank">Kim kundad</a>
+                                            </div>
+                                        </div>
+                                    </footer>
+                                  </div>
+
+
+
 @endsection
