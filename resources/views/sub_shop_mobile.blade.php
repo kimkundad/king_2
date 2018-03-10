69c0d2a5ba7e6904e9ef31f7b52c3de48874c3ae
kimkundad <kim.kundad@gmail.com>
2018-03-11 03:56:24 +0700

    golf

--- resources/views/sub_shop_mobile.blade.php
+++ resources/views/sub_shop_mobile.blade.php
@@ -108,7 +108,7 @@ return "$strDay $strMonthThai $strYear";
                                 รายละเอียดของร้านค้า
                                 <p class="document-subtitle" style="margin-top: 10px;"><b><i class="fa fa-circle-o-notch" ></i> {{$objs->shop_code}}, {{$objs->branders_group}}, {{$objs->branders_type}}</b></p>
                                 <hr style="color:#ddd; margin-top: 0.5rem;  margin-bottom: 0.5rem;">
-                                <p class="document-subtitle" style="font-size: 13px;"><b><i class="fa fa-phone" ></i></b> {{$objs->shop_phone}}</p>
+                                <p class="document-subtitle" style="font-size: 13px;"><b><i class="fa fa-phone" ></i></b> {{$objs->shop_phone}} | <b><i class="fa fa-map-marker" ></i></b> {{$objs->PROVINCE_NAME}}</p>
                                 <p class="document-subtitle" style="font-size: 13px;"><b><i class="fa fa-envelope-o" ></i></b> {{$objs->shop_email}}</p>
                               </div>
 
