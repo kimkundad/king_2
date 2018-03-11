2b3cdc6315d92a3aa28fea551863b092fe3307a2
kimkundad <kim.kundad@gmail.com>
2018-03-11 07:05:17 +0700

    golf

--- resources/views/product_mobile.blade.php
+++ resources/views/product_mobile.blade.php
@@ -288,19 +288,16 @@ return "$strDay $strMonthThai $strYear";
                                     <h6>รูปภาพประกอบสินค้า</h6>
                                     <hr>
 
-
-                                    </div>
-
-
                                     @if($img_all)
                                     @foreach($img_all as $img_u)
-                                    <div class="col-6 col-md-6">
+                                    <div class="col-5 col-md-5">
                                       <a class="example-image-link" href="{{url('admin/assets/gallery_product/'.$img_u->image)}}" >
                                       <img src="{{url('admin/assets/gallery_product/'.$img_u->image)}}" alt="{{$product->product_name}}" class="img-raised mar-bot ">
                                       </a>
                                     </div>
                                     @endforeach
                                     @endif
+                                    </div>
 
 
 
